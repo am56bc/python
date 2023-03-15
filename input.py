eb532e17cd847ce401fff3e807a69d71a6bc58fb
amer <amabor9@gmail.com>
2023-03-15 16:45:56 -0500

    added function,script and statement

---
+++ input.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""this is a script showing user input
+this script is setting the variable age equal to the built-in function input
+the print is str + str + str
+age2 = changing age variable to an int, int + int
+last print is str + str + str 
+"""
+
+
+
+
+
+age = input("enter ur age:\n")
+print('you are ' + age + ' years old')
+
+age2 = int(age) + 50 ##technically dont need this##
+
+print('in 50 years you will be ' + str(age2) + ' years old')
+
+
+
